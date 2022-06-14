a1874855e9a279c3e0dab874e46a66e273be1453
unknown <diegomarks1806@gmail.com>
2022-06-14 19:25:54 -0300

    Primeiro comando que aprendemos no livro, imprimimos a mensagemHello Word

---
+++ Welcome1.java
@@ -0,0 +1,7 @@
+public class Welcome1 {
+    // método main inicia a execução do aplicativo Java
+    public static void main(String[] args) {
+        System.out.println("\"entre aspas\"");
+        System.out.println("Hoje");
+    } // fim do método main
+} // fim da classe Welcome4
